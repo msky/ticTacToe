4a7729f626454d7c1e37fbd0058e552f175d2d1b
Maciej Maciejewski <maciej.maciejewski@capgemini.com>
2019-07-02 20:03:25 +0200

    initial commit

---
+++ src/test/java/msky/ticTacToe/TicTacToeApplicationTests.java
@@ -0,0 +1,16 @@
+package msky.ticTacToe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TicTacToeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
